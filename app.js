ffbb73b4723f383f95148489b399a95b30b4af31
Shohag <popanda49@gmail.com>
2021-03-07 15:45:52 +0600

    app.js,index.html,style.css added

---
+++ app.js
@@ -0,0 +1,39 @@
+
+//FINDING HTML ELEMENT BY ID
+
+
+// let x = document.getElementById('demo');
+// document.getElementById('home').innerHTML=
+// x.innerHTML
+
+// console.log(x.innerHTML)
+
+
+
+//FINDING HTML ELEMENT BY TAG NAME
+
+// let x = document.getElementsByTagName("h1");
+// document.getElementById('home').innerHTML=
+// x[1].innerHTML
+// console.log(x[0].innerHTML)
+
+
+
+//Finding HTML Elements by Class Name
+
+// let x = document.getElementsByClassName('myTurn');
+// document.getElementById('home').innerHTML=
+// x[1].innerText
+
+// console.log(x[1].innerHTML)
+
+
+
+
+
+//Finding HTML Elements by CSS Selectors
+
+
+let x = document.querySelector('div#home');
+
+console.log(x.);
